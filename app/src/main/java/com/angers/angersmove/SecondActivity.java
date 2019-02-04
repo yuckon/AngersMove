05b588b7e8a72951273f8afb2d7bd1101b319e39
Samuel Beslot <samuel.beslot@imie.fr>
2019-02-04 12:01:18 +0100

    Initial Commit

--- app/src/main/java/com/angers/angersmove/SecondActivity.java
+++ app/src/main/java/com/angers/angersmove/SecondActivity.java
@@ -2,22 +2,12 @@ package com.angers.angersmove;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final Button button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View activity_second){
-                setContentView(R.layout.activity_second);
-            }
-        });
+        setContentView(R.layout.activity_second);
     }
     protected void onStart(){
         super.onStart();
---
+++ app/src/main/java/com/angers/angersmove/TAG.java
@@ -0,0 +1,4 @@
+package com.angers.angersmove;
+
+class TAG {
+}
